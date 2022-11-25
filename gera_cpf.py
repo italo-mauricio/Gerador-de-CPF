805ca76ce145430260af403ad894c9e48fc67ff3
italo <italogithub@gmail.com>
2022-11-25 03:37:08 -0300

    criando a janela

---
+++ gera_cpf.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+
+
+
+color1 = "#ff0000"  # red
+color2 = "#008c00"  # green
+color3 = "0000000"  # black
+
+
+window = Tk()
+window.title("")
+
+
+window.mainloop()
+
+
+
+
+
+
