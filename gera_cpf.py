50f29a948673fe4c46ceaf1eaadd3f70e1f20392
italo <italogithub@gmail.com>
2022-11-25 03:54:18 -0300

    adicionando botão

--- gera_cpf.py
+++ gera_cpf.py
@@ -15,11 +15,12 @@ window.geometry("400x200")          # escolho o tamanho da janela
 window.resizable(width= False, height= False)       # impeço do usuário mudar o tamanho da tela
 
 
-text_generator = Label(window,text="CPF Generator", bg=color3, fg=color1, font=('Arial', 20, 'bold'))
+text_generator = Label(window,text="CPF Generator", bg=color3, fg=color1, font=('Ivy',20,'bold')) # definindo o texto, a fonte e a cor da fundo da tela
 text_generator.place(x=75,y=20)
 
 
-
+bnt = Button(window, text="Generate CPF", bg=color2, fg=color3, font=('Arial', 15, 'bold'), relief=RAISED, overrelief=SUNKEN)
+bnt.place(x=150,y=70)
 
 
 
