cbfba95e7c39b28954efaa2893bc56b04b916d76
italo <italogithub@gmail.com>
2022-11-25 03:38:32 -0300

    update

--- gera_cpf.py
+++ gera_cpf.py
@@ -5,11 +5,12 @@ from tkinter import ttk
 
 color1 = "#ff0000"  # red
 color2 = "#008c00"  # green
-color3 = "0000000"  # black
+color3 = "#000000"  # black
 
 
 window = Tk()
 window.title("")
+window.configure(bg=color3)
 
 
 window.mainloop()
