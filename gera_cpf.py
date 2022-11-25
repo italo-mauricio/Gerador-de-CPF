9249e307c8574fd4162eb1e9a47dca9ae1dd65cf
italo <italogithub@gmail.com>
2022-11-25 04:10:23 -0300

    update

--- gera_cpf.py
+++ gera_cpf.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from random import randint
+import gerador_cpf
 
 
 
@@ -18,15 +20,45 @@ window.resizable(width= False, height= False)       # impeço do usuário mudar 
 text_generator = Label(window,text="CPF Generator", bg=color3, fg=color1, font=('Ivy',20,'bold')) # definindo o texto, a fonte e a cor da fundo da tela
 text_generator.place(x=75,y=20)
 
+def gerar():
+        num = str(randint(100000000, 999999999))
 
-bnt = Button(window, text="Generate CPF", bg=color2, fg=color3, font=('Arial', 15, 'bold'), relief=RAISED, overrelief=SUNKEN)
+
+        new_cpf = num
+        reverse = 10
+        total = 0
+
+
+        for i in range(19):
+            if i > 8:
+                i -= 9
+            total += int(new_cpf[i]) * reverse
+
+            reverse -= 1
+            if reverse < 2:
+                reverse = 11
+                d = 11 - (total % 11)
+
+                if d > 9:
+                    d = 0
+                total = 0
+                new_cpf += str(d)
+
+        cpf['text'] = new_cpf
+
+        
+bnt = Button(window, text="Generate CPF", command=gerar, bg=color2, fg=color3, font=('Arial', 15, 'bold'), 
+                    relief=RAISED, overrelief=SUNKEN)
 bnt.place(x=150,y=70)
 
 
 
+cpf = Label(window, text="", bg=color3, fg=color2, font=('Arial', 30, 'bold'))
+cpf.place(x=85,y=120)
+
+
+
 window.mainloop()
-
-
 
 
 
